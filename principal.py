ea8da758456d1b81ddc0ae3cb0eef7a782eae1ff
lucasfreitas25 <lucasfreitas25001@gmail.com>
2025-01-09 08:05:46 -0400

    commit de tudo

--- principal.py
+++ principal.py
@@ -9,11 +9,10 @@ from openpyxl.styles import Font, Border, Side
 from ajustar_planilha import ajustar_colunas, ajustar_bordas
 from Drive import add_arquivos_a_pasta
 
-
-api_estadual = f'https://servicodados.ibge.gov.br/api/v3/agregados/5457/periodos/2013|2014|2015|2016|2017|2018|2019|2020|2021|2022/variaveis/8331|216|214|112?{estadual}&classificacao=782[40092,45982,40099,40101,40102,40136,40104,40137,40138,40139,40106,40143,40145,40112,40114,40149,40150,40151,40152,40261,40118,40119,40262,40263,40120,40121,40122,40266,40269,40124,40125,40271,40126,40127,40273,40274]'
-#api_municipal = f'https://servicodados.ibge.gov.br/api/v3/agregados/5457/periodos/2020|2021|2022/variaveis/8331|216|214|112|215?{municipal}&classificacao=782[40092,45982,40099,40101,40102,40136,40104,40137,40138,40139,40106,40143,40145,40112,40114,40149,40150,40151,40152,40261,40118,40119,40262,40263,40120,40121,40122,40266,40269,40124,40125,40271,40126,40127,40273,40274]'
+api_estadual = f'https://servicodados.ibge.gov.br/api/v3/agregados/5457/periodos/2014|2015|2016|2017|2018|2019|2020|2021|2022|2023/variaveis/8331|216|214|112?{estadual}&classificacao=782[40092,45982,40099,40101,40102,40136,40104,40137,40138,40139,40106,40143,40145,40112,40114,40149,40150,40151,40152,40261,40118,40119,40262,40263,40120,40121,40122,40266,40269,40124,40125,40271,40126,40127,40273,40274]'
 ROOT_PATH = Path(__file__).parent
 
+lista_cod_prod = [40092,45982,40099,40101,40102,40136,40104,40137,40138,40139,40106,40143,40145,40112,40114,40149,40150,40151,40152,40261,40118,40119,40262,40263,40120,40121,40122,40266,40269,40124,40125,40271,40126,40127,40273,40274]
 class TLSAdapter(rq.adapters.HTTPAdapter):
     def init_poolmanager(self, *args, **kwargs):
         ctx = ssl.create_default_context()
@@ -48,16 +47,15 @@ def requisitando_dados(api):
     dados_brutos_216 = dados_brutos[1]
     dados_brutos_214 = dados_brutos[2]
     dados_brutos_112 = dados_brutos[3]
-
     return dados_brutos_8331, dados_brutos_216, dados_brutos_214, dados_brutos_112
 
-def tratando_dados(dados_brutos_8331, dados_brutos_216, dados_brutos_214, dados_brutos_112 ):
+def tratando_dados(dados_brutos_8331, dados_brutos_216, dados_brutos_214, dados_brutos_112):
     dados_limpos_8331 = []
     dados_limpos_216 = []
     dados_limpos_214 = []
     dados_limpos_112 = []
 
-    variaveis = [dados_brutos_8331, dados_brutos_216, dados_brutos_214, dados_brutos_112 ]
+    variaveis = [dados_brutos_8331, dados_brutos_216, dados_brutos_214, dados_brutos_112]
 
     for i in variaveis:
         id_tabela = i['id']
@@ -111,6 +109,25 @@ def tratando_dados(dados_brutos_8331, dados_brutos_216, dados_brutos_214, dados_
 
     return dados_limpos_8331, dados_limpos_216, dados_limpos_214, dados_limpos_112
 
+def executando_municipal():
+    lista_dados_8331 = [] 
+    lista_dados_216 = []
+    lista_dados_214 = []
+    lista_dados_112 = []
+    for codigo in lista_cod_prod:
+        api_municipal = f'https://servicodados.ibge.gov.br/api/v3/agregados/5457/periodos/2013|2014|2015|2016|2017|2018|2019|2020|2021|2022|2023/variaveis/8331|216|214|112?localidades=N6[5100102,5100201,5100250,5100300,5100359,5100409,5100508,5100607,5100805,5101001,5101209,5101258,5101308,5101407,5101605,5101704,5101803,5101852,5101902,5102504,5102603,5102637,5102678,5102686,5102694,5102702,5102793,5102850,5103007,5103056,5103106,5103205,5103254,5103304,5103353,5103361,5103379,5103403,5103437,5103452,5103502,5103601,5103700,5103809,5103858,5103908,5103957,5104104,5104203,5104500,5104526,5104542,5104559,5104609,5104807,5104906,5105002,5105101,5105150,5105176,5105200,5105234,5105259,5105309,5105507,5105580,5105606,5105622,5105903,5106000,5106109,5106158,5106174,5106182,5106190,5106208,5106216,5106224,5106232,5106240,5106257,5106265,5106273,5106281,5106299,5106307,5106315,5106372,5106422,5106455,5106505,5106653,5106703,5106752,5106778,5106802,5106828,5106851,5107008,5107040,5107065,5107107,5107156,5107180,5107198,5107206,5107248,5107263,5107297,5107305,5107354,5107404,5107578,5107602,5107701,5107743,5107750,5107768,5107776,5107792,5107800,5107859,5107875,5107883,5107909,5107925,5107941,5107958,5108006,5108055,5108105,5108204,5108303,5108352,5108402,5108501,5108600,5108808,5108857,5108907,5108956]&classificacao=782[{codigo}]'       
+        variavel8331municipal, variavel_216municipal, variavel214municipal, variavel112municipal = requisitando_dados(api_municipal)
+        if len(variavel8331municipal) == 0 and len(variavel_216municipal) == 0 and len(variavel214municipal) == 0 and len(variavel112municipal) == 0:
+            lista_dados_8331, lista_dados_216, lista_dados_214, lista_dados_112 = tratando_dados(variavel8331municipal, variavel_216municipal,variavel214municipal, variavel112municipal)
+
+        else:
+            novos_dados_8331, novos_dados_216, novos_dados_214, novos_dados_112 = tratando_dados(variavel8331municipal, variavel_216municipal,variavel214municipal, variavel112municipal)
+            lista_dados_8331.extend(novos_dados_8331)
+            lista_dados_216.extend(novos_dados_216)
+            lista_dados_214.extend(novos_dados_214)
+            lista_dados_112.extend(novos_dados_112)
+    
+    return  lista_dados_8331,lista_dados_216,lista_dados_214, lista_dados_112
 def gerando_dataframe(dados_limpos_8331, dados_limpos_216, dados_limpos_214, dados_limpos_112):
 
     df8331 = pd.DataFrame(dados_limpos_8331)
@@ -124,16 +141,15 @@ def gerando_dataframe(dados_limpos_8331, dados_limpos_216, dados_limpos_214, dad
     dataframe['Área plantada ou destinada à colheita'] = dataframe['Área plantada ou destinada à colheita'].astype(float)
     dataframe['Área colhida'] = dataframe['Área colhida'].astype(float)
     dataframe['Quantidade produzida'] = dataframe['Quantidade produzida'].astype(float)
-    dataframe['Rendimento médio da produção'] = dataframe['Rendimento médio da produção'].astype(float)
     return dataframe
 
 def coluna_cultura(dataframe):
     
     culturas_temporarias = ['Abacaxi', 'Algodão herbáceo', 'Amendoim', 'Arroz', 'Batata-doce',
-                            'Cana-de-açúcar', 'Feijão', 'Girassol', 'Mamona', 'Mandioca'
+                            'Cana-de-açúcar', 'Feijão', 'Girassol', 'Mamona', 'Mandioca',
                             'Melancia', 'Melão', 'Milho', 'Soja', 'Sorgo', 'Tomate', 'Trigo']
     
-    culturas_permanentes = ['Açaí', 'Algodão arbóreo', 'Banana', 'Borracha', 'Cacau', 'Café', 'Castanha de caju',
+    culturas_permanentes = ['Açaí', 'Algodão arbóreo', 'Banana', 'Borracha látex coagulado', 'Borracha látex líquido', 'Cacau', 'Café', 'Castanha de caju',
                             'Coco-da-baía', 'Goiaba', 'Guaraná', 'Laranja', 'Limão', 'Mamão', 'Manga',
                             'Maracujá', 'Palmito', 'Pimenta-do-reino', 'Tangerina', 'Urucum', 'Uva']
     
@@ -143,8 +159,6 @@ def coluna_cultura(dataframe):
             dataframe.at[index, 'cultura'] = 'Temporária'
         elif row['produto'] in culturas_permanentes:
             dataframe.at[index, 'cultura'] = 'Permanente'
-        else:
-            dataframe.at[index, 'cultura'] = 'Outros'
     
     return dataframe
 
@@ -155,8 +169,12 @@ dados_limpos_8331, dados_limpos_216, dados_limpos_214, dados_limpos_112 = tratan
 dataframe = gerando_dataframe(dados_limpos_8331, dados_limpos_216, dados_limpos_214, dados_limpos_112)
 df5457_estadual = coluna_cultura(dataframe)
 df5457_estadual.to_excel('C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivos\\DATAHUB\\TABELAS\\TABELAS EM CSV\\PAM_5457_ESTADUAL.xlsx', index=False)
-df5457_estadual.to_csv('C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivos\\DATAHUB\\TABELAS\\TABELAS EM CSV\\PAM_5457_ESTADUAL.csv', index=False)
 #print(df5457_estadual)
+dados_limpos_8331_muni, dados_limpos_216_muni, dados_limpos_214_muni, dados_limpos_112_muni = executando_municipal()
+dataframe_muni = gerando_dataframe(dados_limpos_8331_muni, dados_limpos_216_muni, dados_limpos_214_muni, dados_limpos_112_muni)
+df5457_municipal = coluna_cultura(dataframe_muni)
+df5457_municipal.to_excel('C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivos\\DATAHUB\\TABELAS\\TABELAS EM CSV\\PAM_5457_MUNICIPAL.xlsx', index=False)
+
 
 # CARREGA A PLANILHA DO PAM 5457 E FAZ AS ALTERAÇÕES ESTRUTURAIS DA PLANILHA
 
@@ -175,45 +193,36 @@ wb_5457_estadual.save("C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivo
 #TESTANDO JUNTAR TODAS PLANILHAS EM UMA SÓ
 planilha_principal = openpyxl.Workbook()
 wb_5457_estadual = openpyxl.load_workbook("C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivos\\DATAHUB\\TABELAS\\TABELAS EM CSV\\PAM_5457_ESTADUAL.xlsx")  
+wb_5457_municipal = openpyxl.load_workbook("C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivos\\DATAHUB\\TABELAS\\TABELAS EM CSV\\PAM_5457_MUNICIPAL.xlsx")  
 aba_5457_estadual = planilha_principal.create_sheet("PAM 5457 ESTADUAL")
+aba_5457_municipal = planilha_principal.create_sheet("PAM 5457 MUNICIPAL")
 
 
 # Copiar os dados da segunda planilha para a nova planilha
 for linha in wb_5457_estadual.active.iter_rows(values_only=True):
     aba_5457_estadual.append(linha)
+    
+for linha in wb_5457_municipal.active.iter_rows(values_only=True):
+    aba_5457_municipal.append(linha)
 
 for aba in planilha_principal.sheetnames:
-    if aba not in ["PAM 5457 ESTADUAL"]:
+    if aba not in ["PAM 5457 ESTADUAL", "PAM 5457 MUNICIPAL"]:
         del planilha_principal[aba]
 
 
-colunas_para_ajustar = ['B', 'C', 'D', 'F', 'G', 'J']
-largura_desejada = 22
-
-for coluna in colunas_para_ajustar:
-    aba_5457_estadual.column_dimensions[coluna].width = largura_desejada
-
-colunas_maiores = ['E', 'H', 'I']
-largura_planejada = 35
-
-for coluna in colunas_maiores:
-    aba_5457_estadual.column_dimensions[coluna].width = largura_planejada
+lista_abas = [aba_5457_estadual, aba_5457_municipal]
+for abas in lista_abas:
+    ajustar_colunas(abas)
 
 
 planilha_principal.save("C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivos\\DATAHUB\\TABELAS\\TABELAS EM CSV\\PAM.xlsx")
 worksheet = planilha_principal.active
 df = pd.read_excel('C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivos\\DATAHUB\\TABELAS\\TABELAS EM CSV\\PAM.xlsx')
 
-for sheet_name in planilha_principal.sheetnames:
-    worksheet = planilha_principal[sheet_name]
-    
-    for col_num in range(1, worksheet.max_column + 1):
-        cell = worksheet.cell(row=1, column=col_num)
-        cell.font = Font(bold=True)
-        cell.border = Border(left=Side(style='thin'), right=Side(style='thin'), top=Side(style='thin'), bottom=Side(style='thin'))
+ajustar_bordas(planilha_principal)
 
 planilha_principal.save("C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivos\\DATAHUB\\TABELAS\\TABELAS EM CSV\\PAM.xlsx")
-
+print("ESTOU EXECUTANDO SQL.PY")
 '''
 #Faz autenticação do google drive para jogar os arquivos gerados no codigo python
 CLIENT_SECRET_FILE = 'credencials.json'
@@ -230,8 +239,6 @@ MIME_TYPES = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
 
 add_arquivos_a_pasta(FILE_NAMES, MIME_TYPES, service, file_id)
 '''
-
-
 #ABRE O ARQUIVO SQL.PY E EXECUTA TODOS OS COMANDOS DENTRO DELE
 if __name__ == '__main__':
     from sql import executar_sql 
--- sql.py
+++ sql.py
@@ -1,6 +1,7 @@
-from principal import df5457_estadual
+from principal import df5457_estadual,df5457_municipal
 import psycopg2
 from conexão import conexao
+
 def executar_sql():
     
     cursor = conexao.cursor()
@@ -22,14 +23,39 @@ def executar_sql():
         '''
     cursor.execute(criando_tabela_estadual)
     
+    criando_tabela_municipal = f'''
+        CREATE TABLE IF NOT EXISTS pam.PAM_5457_MUNICIPAL (
+            id_pam_5457_municipal SERIAL PRIMARY KEY,
+            id INTEGER,
+            nome TEXT,
+            id_produto INTEGER,
+            produto TEXT, 
+            area_plantada INTEGER,
+            area_colhida INTEGER,
+            quantidade_produzida INTEGER, 
+            rendimento_producao INTEGER, 
+            ano DATE,
+            cultura TEXT);
+        '''
+    cursor.execute(criando_tabela_municipal)
+    
     verificando_existencia_estadual = '''
     SELECT 1
     FROM information_schema.tables
     WHERE table_type='BASE TABLE' AND table_name='pam_5457_estadual';
     '''
+    verificando_existencia_municipal = '''
+    SELECT 1
+    FROM information_schema.tables
+    WHERE table_type='BASE TABLE' AND table_name='pam_5457_municipal';
+    '''
     
     cursor.execute(verificando_existencia_estadual)
     resultado_estadual = cursor.fetchone()
+    
+
+    cursor.execute(verificando_existencia_municipal)
+    resultado_municipal = cursor.fetchone()
     # Verifique se as tabelas existem e exclua todos dados da tabela
 
     if resultado_estadual[0] == 1:
@@ -37,11 +63,17 @@ def executar_sql():
         TRUNCATE PAM_5457_ESTADUAL;
         '''
         cursor.execute(dropando_tabela_estadual)
+        
+    if resultado_municipal[0] == 1:
+        dropando_tabela_municipal = '''
+        TRUNCATE PAM_5457_MUNICIPAL;
+        '''
+        cursor.execute(dropando_tabela_municipal)
 
 
     inserindo_dados_estadual = '''
-    INSERT INTO pam.PAM_5457_ESTADUAL (id, nome, id_produto, produto, area_plantada, area_colhida, quantidade_produzida, rendimento_producao, ano)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    INSERT INTO pam.PAM_5457_ESTADUAL (id, nome, id_produto, produto, area_plantada, area_colhida, quantidade_produzida, rendimento_producao, ano, cultura)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
     '''
     try:
         for idx, i in df5457_estadual.iterrows():
@@ -54,7 +86,8 @@ def executar_sql():
                 i['Área colhida'],
                 i['Quantidade produzida'],
                 i['Rendimento médio da produção'],
-                i['ano']
+                i['ano'],
+                i['cultura']
             )
             cursor.execute(inserindo_dados_estadual, dados)
             conexao.commit()
@@ -62,17 +95,54 @@ def executar_sql():
     except psycopg2.Error as e:
         print(f"Erro ao inserir dados estaduais: {e}")
 
-    atualizando_cultura_estadual = '''
-    UPDATE pam.PAM_5457_ESTADUAL
-    SET cultura = %s
-    WHERE produto = %s AND ano = %s;
+    
+    inserindo_dados_municipal = '''
+    INSERT INTO pam.PAM_5457_MUNICIPAL (id, nome, id_produto, produto, area_plantada, area_colhida, quantidade_produzida, rendimento_producao, ano, cultura)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
     '''
+    try:
+        for idx, i in df5457_municipal.iterrows():
+            dados = (
+                i['id'],
+                i['nome'],
+                i['id_produto'],
+                i['produto'],
+                i['Área plantada ou destinada à colheita'],
+                i['Área colhida'],
+                i['Quantidade produzida'],
+                i['Rendimento médio da produção'],
+                i['ano'],
+                i['cultura']
+            )
+            cursor.execute(inserindo_dados_municipal, dados)
+            conexao.commit()
 
-    for idx, i in df5457_estadual.iterrows():
-        cultura = i['cultura']  # Substitua 'cultura' pelo nome real da coluna em seu DataFrame
-        produto = i['produto']
-        ano = i['ano']
-        cursor.execute(atualizando_cultura_estadual, (cultura, produto, ano))
+    except psycopg2.Error as e:
+        print(f"Erro ao inserir dados municipais: {e}")
+
+    # atualizando_cultura_estadual = '''
+    # UPDATE pam.PAM_5457_ESTADUAL
+    # SET cultura = %s
+    # WHERE produto = %s AND ano = %s;
+    # '''
+
+    # for idx, i in df5457_estadual.iterrows():
+    #     cultura = i['cultura'] 
+    #     produto = i['produto']
+    #     ano = i['ano']
+    #     cursor.execute(atualizando_cultura_estadual, (cultura, produto, ano))
+        
+    # atualizando_cultura_municipal = '''
+    # UPDATE pam.PAM_5457_MUNICIPAL
+    # SET cultura = %s
+    # WHERE produto = %s AND ano = %s;
+    # '''
+
+    # for idx, i in df5457_municipal.iterrows():
+    #     cultura = i['cultura']  
+    #     produto = i['produto']
+    #     ano = i['ano']
+    #     cursor.execute(atualizando_cultura_municipal, (cultura, produto, ano))
 
     conexao.commit()
     conexao.close()
