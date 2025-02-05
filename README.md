903744e0d72ca94f788cb3504599c12422a2ce75
lucasfreitas25 <lucasfreitas25001@gmail.com>
2025-02-05 07:33:04 -0400

    ADD README

---
+++ README.md
@@ -0,0 +1,44 @@
+<h1 align="center"> Painel automatizado PAM </h1>
+
+<p align="center">
+Sistema automatizado de extração e transformação de dados envolvendo a parte agrícola de MT <br/>
+
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;
+
+
+
+<br>
+
+<p align="center">
+  <img alt="Painel_Pam" src="Print_painel.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- Python
+- PowerBI
+- Git e Github
+- PostgreSQL
+- Requests, Pandas, Openpyxl
+
+## 💻 Projeto
+
+O processo consistia em realizar:
+<p>
+Extract, Transform, Loading dos dados da Pesquisa Agrícola Municipal  
+</p>
+<p>
+Deixar todo o processo manual de maneira automatização desda extração até na atualização do painel de dados
+</p>
+<p>
+Construir o dashboard para apresentação para partes interessadas
+</p>
+<p>
+Desenvolvido em conjunto com Vinicius Hideki
+</p>
--- principal.py
+++ principal.py
@@ -1,3 +1,4 @@
+import datetime
 from pathlib import Path
 import pandas as pd
 import requests as rq 
@@ -9,7 +10,6 @@ from openpyxl.styles import Font, Border, Side
 from ajustar_planilha import ajustar_colunas, ajustar_bordas
 from Drive import add_arquivos_a_pasta
 
-api_estadual = f'https://servicodados.ibge.gov.br/api/v3/agregados/5457/periodos/2014|2015|2016|2017|2018|2019|2020|2021|2022|2023/variaveis/8331|216|214|112?{estadual}&classificacao=782[40092,45982,40099,40101,40102,40136,40104,40137,40138,40139,40106,40143,40145,40112,40114,40149,40150,40151,40152,40261,40118,40119,40262,40263,40120,40121,40122,40266,40269,40124,40125,40271,40126,40127,40273,40274]'
 ROOT_PATH = Path(__file__).parent
 
 lista_cod_prod = [40092,45982,40099,40101,40102,40136,40104,40137,40138,40139,40106,40143,40145,40112,40114,40149,40150,40151,40152,40261,40118,40119,40262,40263,40120,40121,40122,40266,40269,40124,40125,40271,40126,40127,40273,40274]
@@ -37,8 +37,12 @@ def requisitando_dados(api):
         raise Exception(f"Erro ao analisar a resposta JSON da API: {str(e)}")
 
     # Verificação se a resposta contém os dados esperados
-    if len(dados_brutos) < 4:
-        raise Exception("A resposta da API não contém dados suficientes.")
+    if len(dados_brutos) < 4:   
+        dados_brutos_8331 = None
+        dados_brutos_216 = None
+        dados_brutos_214 = None
+        dados_brutos_112 = None
+        return dados_brutos_8331, dados_brutos_216, dados_brutos_214, dados_brutos_112
     
     if dados_brutos_api.status_code == 500:
         raise Exception(f"Os dados passou de 100.0000 por isso o codigo de: {dados_brutos_api.status_code}")
@@ -109,23 +113,29 @@ def tratando_dados(dados_brutos_8331, dados_brutos_216, dados_brutos_214, dados_
 
     return dados_limpos_8331, dados_limpos_216, dados_limpos_214, dados_limpos_112
 
-def executando_municipal():
+
+ano_atual = datetime.datetime.now().year
+def executando_dados(tipo):
     lista_dados_8331 = [] 
     lista_dados_216 = []
     lista_dados_214 = []
     lista_dados_112 = []
     for codigo in lista_cod_prod:
-        api_municipal = f'https://servicodados.ibge.gov.br/api/v3/agregados/5457/periodos/2013|2014|2015|2016|2017|2018|2019|2020|2021|2022|2023/variaveis/8331|216|214|112?localidades=N6[5100102,5100201,5100250,5100300,5100359,5100409,5100508,5100607,5100805,5101001,5101209,5101258,5101308,5101407,5101605,5101704,5101803,5101852,5101902,5102504,5102603,5102637,5102678,5102686,5102694,5102702,5102793,5102850,5103007,5103056,5103106,5103205,5103254,5103304,5103353,5103361,5103379,5103403,5103437,5103452,5103502,5103601,5103700,5103809,5103858,5103908,5103957,5104104,5104203,5104500,5104526,5104542,5104559,5104609,5104807,5104906,5105002,5105101,5105150,5105176,5105200,5105234,5105259,5105309,5105507,5105580,5105606,5105622,5105903,5106000,5106109,5106158,5106174,5106182,5106190,5106208,5106216,5106224,5106232,5106240,5106257,5106265,5106273,5106281,5106299,5106307,5106315,5106372,5106422,5106455,5106505,5106653,5106703,5106752,5106778,5106802,5106828,5106851,5107008,5107040,5107065,5107107,5107156,5107180,5107198,5107206,5107248,5107263,5107297,5107305,5107354,5107404,5107578,5107602,5107701,5107743,5107750,5107768,5107776,5107792,5107800,5107859,5107875,5107883,5107909,5107925,5107941,5107958,5108006,5108055,5108105,5108204,5108303,5108352,5108402,5108501,5108600,5108808,5108857,5108907,5108956]&classificacao=782[{codigo}]'       
-        variavel8331municipal, variavel_216municipal, variavel214municipal, variavel112municipal = requisitando_dados(api_municipal)
-        if len(variavel8331municipal) == 0 and len(variavel_216municipal) == 0 and len(variavel214municipal) == 0 and len(variavel112municipal) == 0:
-            lista_dados_8331, lista_dados_216, lista_dados_214, lista_dados_112 = tratando_dados(variavel8331municipal, variavel_216municipal,variavel214municipal, variavel112municipal)
-
-        else:
-            novos_dados_8331, novos_dados_216, novos_dados_214, novos_dados_112 = tratando_dados(variavel8331municipal, variavel_216municipal,variavel214municipal, variavel112municipal)
-            lista_dados_8331.extend(novos_dados_8331)
-            lista_dados_216.extend(novos_dados_216)
-            lista_dados_214.extend(novos_dados_214)
-            lista_dados_112.extend(novos_dados_112)
+        for ano in range(2014, ano_atual):
+            if tipo == 'estadual':
+                api = f'https://servicodados.ibge.gov.br/api/v3/agregados/5457/periodos/{ano}/variaveis/8331|216|214|112?{estadual}&classificacao=782[{codigo}]'
+            else:
+                api = f'https://servicodados.ibge.gov.br/api/v3/agregados/5457/periodos/{ano}/variaveis/8331|216|214|112?localidades=N6[5100102,5100201,5100250,5100300,5100359,5100409,5100508,5100607,5100805,5101001,5101209,5101258,5101308,5101407,5101605,5101704,5101803,5101852,5101902,5102504,5102603,5102637,5102678,5102686,5102694,5102702,5102793,5102850,5103007,5103056,5103106,5103205,5103254,5103304,5103353,5103361,5103379,5103403,5103437,5103452,5103502,5103601,5103700,5103809,5103858,5103908,5103957,5104104,5104203,5104500,5104526,5104542,5104559,5104609,5104807,5104906,5105002,5105101,5105150,5105176,5105200,5105234,5105259,5105309,5105507,5105580,5105606,5105622,5105903,5106000,5106109,5106158,5106174,5106182,5106190,5106208,5106216,5106224,5106232,5106240,5106257,5106265,5106273,5106281,5106299,5106307,5106315,5106372,5106422,5106455,5106505,5106653,5106703,5106752,5106778,5106802,5106828,5106851,5107008,5107040,5107065,5107107,5107156,5107180,5107198,5107206,5107248,5107263,5107297,5107305,5107354,5107404,5107578,5107602,5107701,5107743,5107750,5107768,5107776,5107792,5107800,5107859,5107875,5107883,5107909,5107925,5107941,5107958,5108006,5108055,5108105,5108204,5108303,5108352,5108402,5108501,5108600,5108808,5108857,5108907,5108956]&classificacao=782[{codigo}]'       
+            print(api)
+            variavel8331municipal, variavel_216municipal, variavel214municipal, variavel112municipal = requisitando_dados(api)
+            if variavel8331municipal == None and variavel_216municipal == None and variavel214municipal == None and variavel112municipal == None:
+                break
+            else:
+                novos_dados_8331, novos_dados_216, novos_dados_214, novos_dados_112 = tratando_dados(variavel8331municipal, variavel_216municipal,variavel214municipal, variavel112municipal)
+                lista_dados_8331.extend(novos_dados_8331)
+                lista_dados_216.extend(novos_dados_216)
+                lista_dados_214.extend(novos_dados_214)
+                lista_dados_112.extend(novos_dados_112)
     
     return  lista_dados_8331,lista_dados_216,lista_dados_214, lista_dados_112
 def gerando_dataframe(dados_limpos_8331, dados_limpos_216, dados_limpos_214, dados_limpos_112):
@@ -164,13 +174,12 @@ def coluna_cultura(dataframe):
 
 
 #PARTE ESTADUAL
-dados_brutos_8331, dados_brutos_216, dados_brutos_214, dados_brutos_112 = requisitando_dados(api_estadual)
-dados_limpos_8331, dados_limpos_216, dados_limpos_214, dados_limpos_112 = tratando_dados(dados_brutos_8331, dados_brutos_216, dados_brutos_214, dados_brutos_112)
+dados_limpos_8331, dados_limpos_216, dados_limpos_214, dados_limpos_112 = executando_dados('estadual')
 dataframe = gerando_dataframe(dados_limpos_8331, dados_limpos_216, dados_limpos_214, dados_limpos_112)
 df5457_estadual = coluna_cultura(dataframe)
 df5457_estadual.to_excel('C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivos\\DATAHUB\\TABELAS\\TABELAS EM CSV\\PAM_5457_ESTADUAL.xlsx', index=False)
 #print(df5457_estadual)
-dados_limpos_8331_muni, dados_limpos_216_muni, dados_limpos_214_muni, dados_limpos_112_muni = executando_municipal()
+dados_limpos_8331_muni, dados_limpos_216_muni, dados_limpos_214_muni, dados_limpos_112_muni = executando_dados('muni')
 dataframe_muni = gerando_dataframe(dados_limpos_8331_muni, dados_limpos_216_muni, dados_limpos_214_muni, dados_limpos_112_muni)
 df5457_municipal = coluna_cultura(dataframe_muni)
 df5457_municipal.to_excel('C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivos\\DATAHUB\\TABELAS\\TABELAS EM CSV\\PAM_5457_MUNICIPAL.xlsx', index=False)
@@ -179,7 +188,6 @@ df5457_municipal.to_excel('C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arq
 # CARREGA A PLANILHA DO PAM 5457 E FAZ AS ALTERAÇÕES ESTRUTURAIS DA PLANILHA
 
 wb_5457_estadual = openpyxl.load_workbook("C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivos\\DATAHUB\\TABELAS\\TABELAS EM CSV\\PAM_5457_ESTADUAL.xlsx")  
-
 ws_5457_estadual = wb_5457_estadual.active
 
 lista_ws = [ws_5457_estadual]
@@ -187,7 +195,6 @@ lista_wb = [wb_5457_estadual]
 for ws, wb in zip(lista_ws, lista_wb):
     ajustar_colunas(ws)
     ajustar_bordas(wb)
-
 wb_5457_estadual.save("C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivos\\DATAHUB\\TABELAS\\TABELAS EM CSV\\PAM_5457_ESTADUAL.xlsx")
 
 #TESTANDO JUNTAR TODAS PLANILHAS EM UMA SÓ
@@ -223,7 +230,14 @@ ajustar_bordas(planilha_principal)
 
 planilha_principal.save("C:\\Users\\LucasFreitas\\Documents\\Lucas Freitas Arquivos\\DATAHUB\\TABELAS\\TABELAS EM CSV\\PAM.xlsx")
 print("ESTOU EXECUTANDO SQL.PY")
-'''
+
+#ABRE O ARQUIVO SQL.PY E EXECUTA TODOS OS COMANDOS DENTRO DELE
+if __name__ == '__main__':
+    from sql import executar_sql 
+    executar_sql()
+
+    
+    '''
 #Faz autenticação do google drive para jogar os arquivos gerados no codigo python
 CLIENT_SECRET_FILE = 'credencials.json'
 API_NAME = 'drive'
@@ -239,8 +253,3 @@ MIME_TYPES = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
 
 add_arquivos_a_pasta(FILE_NAMES, MIME_TYPES, service, file_id)
 '''
-#ABRE O ARQUIVO SQL.PY E EXECUTA TODOS OS COMANDOS DENTRO DELE
-if __name__ == '__main__':
-    from sql import executar_sql 
-    executar_sql()
-
